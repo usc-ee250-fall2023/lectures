bc55d6c85d8c1082e42acd1a0c23bbfb92287ea6
Mark Redekopp <redekopp@usc.edu>
2023-09-06 09:20:33 -0700

    updated for 2023

--- u3-transport/http/req.py
+++ u3-transport/http/req.py
@@ -1,18 +1,17 @@
 import requests
 import json
 
-r = requests.get('https://api.github.com/orgs/usc-ee250-fall2022/repos')
+r = requests.get('https://api.github.com/orgs/usc-ee250-fall2023/repos')
 print("Headers:\n" + str(r.headers))
 
 if r.status_code == 200:
     repos = r.json()
     for repo in repos:
         print("\n")
-        #print(repo['name'])
-        print(repo)
-        
-#        for k,v in repo.items():
-#            print("\t",k,":",v)
+        print(repo['name'])
+        #print(repo)
+        #for k,v in repo.items():
+        #    print("\t",k,":",v)
 
 elif r.status_code == 404:
     print("URI not found")
